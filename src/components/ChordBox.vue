c9dd4598d26de9456d071a3435d16e77444d7b02
Addie Jackson <addieljackson3@gmail.com>
2020-06-28 12:52:41 -0400

    auto resize chord box

---
+++ src/components/ChordBox.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <p>Addie's ChordBox Yo</p>
+    <form>
+      <div>
+        <label for="chordbox">Chord:</label>
+        <input
+          type="text"
+          :size="chordBoxSize"
+          name="chordbox"
+          v-model="chordInput"
+          @input="resizeInput"
+        />
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    chordBoxSize: 1,
+    chordInput: ""
+  }),
+  methods: {
+    resizeInput() {
+      this.chordBoxSize = this.chordInput.length;
+      if (this.chordBoxSize <= 1) {
+        this.chordBoxSize = 1;
+      }
+    }
+  }
+};
+</script>
+
+<style>
+input[type="text"] {
+  font-size: 16px;
+  font-family: monospace, "Courier New";
+}
+</style>
