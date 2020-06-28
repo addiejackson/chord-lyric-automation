f6b0f2c6bb8c170b620fa3cb8ffcf1eb0be76e70
Gus Vieweg <awvieweg@gmail.com>
2020-06-28 12:52:43 -0400

    base infrastructure

---
+++ src/components/ComboLine.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hello">
+    <div v-for="(lyric, idx) in lyrics" :key="idx">
+      <ChordLine /><br />
+      <LyricLine :lyric="lyric" />
+    </div>
+  </div>
+</template>
+
+<script>
+import ChordLine from "@/components/ChordLine.vue";
+import LyricLine from "@/components/LyricLine.vue";
+export default {
+  name: "ComboLine",
+  props: {
+    lyrics: Array
+  },
+  components: {
+    ChordLine,
+    LyricLine
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {
+  font-family: "Courier New", Courier, monospace;
+  text-align: left;
+}
+</style>
---
+++ src/components/LyricLine.vue
@@ -0,0 +1,15 @@
+<template>
+  <span>{{ lyric }}</span>
+</template>
+
+<script>
+export default {
+  name: "LyricLine",
+  props: {
+    lyric: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
