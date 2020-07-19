9ba52d9e18932a5f5c85c38a0de1d20095dd9fed
Gus Vieweg <awvieweg@gmail.com>
2020-07-19 17:34:14 -0400

    transpose hotfix

--- src/components/ChordBox.vue
+++ src/components/ChordBox.vue
@@ -8,6 +8,7 @@
       v-model="chordInput"
       @input="resizeInput"
       class="chordBox"
+      autocomplete="off"
     />
   </span>
 </template>
@@ -48,7 +49,7 @@ export default {
         this.chordBoxSize = 1;
       }
       if (!this.isolateKey() && this.chordInput) {
-        this.chordBoxAlert = "background-color:#DB848D;";
+        this.chordBoxAlert = "background-color:#DB848D !important;";
         this.originalKey = null;
         this.$emit("disableTranspose", true);
       } else {
@@ -60,12 +61,14 @@ export default {
     isolateKey() {
       let root = this.chordInput.substring(0, 2);
       if (this.keys.includes(root)) {
-        this.accountForAccidentals();
         this.chordSuffix = this.chordInput.substring(2);
         return root;
       }
-      if (this.keys.includes(root[0])) {
-        this.accountForAccidentals();
+      if (
+        this.keys.includes(root[0]) &&
+        ((root[1] != "#" && this.accidental == "flat") ||
+          (root[1] != "b" && this.accidental == "sharp"))
+      ) {
         this.chordSuffix = this.chordInput.substring(1);
         return root[0];
       }
@@ -88,10 +91,30 @@ export default {
         this.chordBoxSize = this.chordInput.length;
         this.$emit("chordEntered", this.chordInput);
       }
+    }
+  },
+  mounted() {
+    console.log("mounted");
+    this.chordInput = this.chord;
+    this.originalKey = this.isolateKey();
+    console.log(this.originalKey);
+    this.resizeInput();
+  },
+  watch: {
+    transposeN: function() {
+      this.transpose(this.transposeN);
     },
-    accountForAccidentals() {
+    chord: function() {
+      if (!this.originalKey) {
+        this.originalKey = this.isolateKey();
+      }
+      this.chordInput = this.chord;
+      this.resizeInput();
+    },
+    accidental: function() {
       let key = this.isolateKey();
       let keyPos = this.keys.indexOf(key);
+      let originalKeyPos = this.keys.indexOf(this.originalKey);
       if (this.accidental == "sharp") {
         this.keys = [
           "C",
@@ -123,31 +146,8 @@ export default {
           "B"
         ];
       }
-      return (key = this.keys[keyPos]);
-    }
-  },
-  mounted() {
-    console.log("mounted");
-    this.chordInput = this.chord;
-    this.originalKey = this.isolateKey();
-    console.log(this.originalKey);
-    this.resizeInput();
-  },
-  watch: {
-    transposeN: function() {
-      this.transpose(this.transposeN);
-    },
-    chord: function() {
-      if (!this.originalKey) {
-        this.originalKey = this.isolateKey();
-      }
-      this.chordInput = this.chord;
-      this.resizeInput();
-    },
-    accidental: function() {
-      let key = this.accountForAccidentals();
-      let originalKeyPos = this.keys.indexOf(this.originalKey);
       if (key) {
+        key = this.keys[keyPos];
         this.chordInput = key + this.chordSuffix;
         this.transposedKey = key;
         this.originalKey = this.keys[originalKeyPos];
